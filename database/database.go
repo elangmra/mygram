d4f8588c2e0bf7dadf0b1e69f21330baa09726cc
Elang Mochamad Riefki Ardian <elangmra@gmail.com>
2024-03-23 13:36:43 +0800

    change db gorm

--- database/database.go
+++ database/database.go
@@ -1,14 +1,13 @@
 package database
 
 import (
-    "database/sql"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
     "log"
     "os"
-
-    _ "github.com/go-sql-driver/mysql"
 )
 
-var db *sql.DB
+var db *gorm.DB
 
 func StartDB() {
     // Read DATABASE_URL from environment variable
@@ -16,20 +15,14 @@ func StartDB() {
 
     // Open database connection
     var err error
-    db, err = sql.Open("mysql", dbURL)
+    db, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{})
     if err != nil {
         log.Fatal("Error connecting to the database:", err)
-    }
-
-    // Ping the database to verify the connection
-    err = db.Ping()
-    if err != nil {
-        log.Fatal("Error pinging the database:", err)
     }
 
     log.Println("Connected to the database")
 }
 
-func GetDB() *sql.DB {
+func GetDB() *gorm.DB {
     return db
 }
